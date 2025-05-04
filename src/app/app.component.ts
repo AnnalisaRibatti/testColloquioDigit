7f681607b579019884af24ef07c4009b30279f72
AnnalisaRibatti <ribattiannalisa@gmail.com>
2025-05-04 14:04:01 +0200

    leftRail

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -12,13 +12,18 @@ export class AppComponent {
   title = 'testColloquioDigit';
 
   selectedImage!: Image;
+  images!: Image[];
+  listImages!: string[]
 
   constructor(private apiPexelsService: ApiPexelsService) {}
 
   ngOnInit(): void {
     this.apiPexelsService.getImage().subscribe( (response: PexelsResponse) => {
       console.log(response);
+      this.images = response.photos;
       this.selectedImage = response.photos[0]
+
+      this.listImages = this.images.map(photo => photo.alt);
     });
   }
 }
--- src/app/components/left-rail/left-rail.component.ts
+++ src/app/components/left-rail/left-rail.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component, Input } from '@angular/core';
 
 @Component({
   selector: 'app-left-rail',
@@ -7,5 +7,6 @@ import { Component } from '@angular/core';
   styleUrl: './left-rail.component.css'
 })
 export class LeftRailComponent {
-
+  @Input()
+  list!: string[]
 }
